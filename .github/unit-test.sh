c517b9da4d6ef6489201f528b88250c571909e4a
aiioana <adi.ioana91@gmail.com>
2022-05-16 13:19:38 +0300

    a

--- .github/unit-test.sh
+++ .github/unit-test.sh
@@ -10,6 +10,6 @@ else
     echo Successfully authorized the target org!
 fi
 echo
-execution_id=$(sfdx force:data:soql:query -q "SELECT AsyncApexJobId FROM ApexTestRunResult where Status='Completed' order by EndTime desc limit 1" -t --username $USERNAME)
+execution_id=$(sfdx force:data:soql:query -q "SELECT AsyncApexJobId FROM ApexTestRunResult where Status='Completed' order by EndTime desc limit 1" -t --username "$USERNAME")
 execution_report=$(echo "$execution_id" | grep ^707*)
-(sfdx force:apex:test:report -i $execution_report --username $USERNAME | sed '/ Pass /d' | sed 's/Test Results/Failed Tests/') &> report.txt
+(sfdx force:apex:test:report -i $execution_report --username "$USERNAME" | sed '/ Pass /d' | sed 's/Test Results/Failed Tests/') &> report.txt
