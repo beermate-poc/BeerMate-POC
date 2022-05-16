4875570c109557b9db209babdce3febd7fdefbe4
aiioana <adi.ioana91@gmail.com>
2022-05-16 11:57:21 +0300

    a

--- .github/unit-test.sh
+++ .github/unit-test.sh
@@ -6,20 +6,20 @@
 # Authorize the target org & abort the pipeline if auth is failing 
 echo "$SERVER_KEY" > server.key
 sfdx force:auth:jwt:grant --clientid $CLIENTID --jwtkeyfile=server.key --username $USERNAME --instanceurl $URL --json > auth.txt
-# if grep -R "Error" auth.txt
-# then
-#     echo The Authorize Org step has failed! Please contact the release manager.
-#     exit 1
-# else
-#     echo Successfully authorized the target org!
-# fi
-# echo 
+if grep -R "Error" auth.txt
+then
+    echo The Authorize Org step has failed! Please contact the release manager.
+    exit 1
+else
+    echo Successfully authorized the target org!
+fi
+echo 
 
 # Identify the delta changes
 # SFDX_MAX_QUERY_LIMIT=30000 sfdx force:apex:test:run --codecoverage --resultformat human -u $USERNAME
 execution_id=`sfdx force:data:soql:query -q "SELECT AsyncApexJobId FROM ApexTestRunResult where Status='Completed' order by EndTime desc limit 1" -t -u $USERNAME`
 execution_report=`echo "$execution_id" | grep ^707*`
-sfdx force:apex:test:report -i $execution_report -u adrian.ioana@molsoncoors.com.prod.uat > tempreport1.txt
+sfdx force:apex:test:report -i $execution_report -u $USERNAME > tempreport1.txt
 sed '/ Pass /d' tempreport1.txt > tempreport2.txt
 sed 's/Test Results/Failed Tests/' tempreport2.txt > report.txt
 
