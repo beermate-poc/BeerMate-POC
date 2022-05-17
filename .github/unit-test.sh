9253c7c163ab39dfca360c8a288c96c12ecd40c9
aiioana <adi.ioana91@gmail.com>
2022-05-17 16:25:34 +0300

    corrected bash

--- .github/unit-test.sh
+++ .github/unit-test.sh
@@ -2,7 +2,7 @@
 
 # Authorize the target org
 echo "$SERVER_KEY" > server.key
-sfdx force:auth:jwt:grant --clientid $CLIENTID --jwtkeyfile=server.key --username $USERNAME --instanceurl $URL --json > auth.txt
+sfdx force:auth:jwt:grant --clientid "$CLIENTID" --jwtkeyfile=server.key --username "$USERNAME" --instanceurl "$URL" --json > auth.txt
 if grep -R "Error" auth.txt
 then
     echo The Authorize Org step has failed! Please contact the release manager.
@@ -13,9 +13,8 @@ fi
 echo
 
 # Run the tests and export the result
-execution_id=`sfdx force:data:soql:query -q "SELECT AsyncApexJobId FROM ApexTestRunResult where Status='Completed' order by EndTime desc limit 1" -t -u $USERNAME`
-echo The execution initial ID is $execution_id
-execution_report=$(echo "$execution_id" | grep ^707*)
-echo The execution final ID is $execution_report
+execution_id=$(sfdx force:data:soql:query -q "SELECT AsyncApexJobId FROM ApexTestRunResult where Status='Completed' order by EndTime desc limit 1" -t -u "$USERNAME")
+echo The execution initial ID is "$execution_id"
+execution_report=$(echo "$execution_id" | grep "^707*")
+echo The execution final ID is "$execution_report"
 (sfdx force:apex:test:report -i "$execution_report" -u "$USERNAME" | sed '/ Pass /d' | sed 's/Test Results/Failed Tests/') &> report.txt
-
