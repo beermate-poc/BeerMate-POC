bc3f8ba6bb88c44ea593390cf77eb49e33f1ed9a
adrianioana <adi.ioana91@gmail.com>
2022-05-26 12:09:32 +0300

    unit test

--- .github/unit-test.sh
+++ .github/unit-test.sh
@@ -12,14 +12,25 @@ else
 fi
 echo
 
-# Run the tests and export the result
-# SFDX_MAX_QUERY_LIMIT=30000 sfdx force:apex:test:run --codecoverage --resultformat human -u "$USERNAME"
-sfdx force:data:soql:query -q "SELECT AsyncApexJobId FROM ApexTestRunResult where Status='Completed' order by EndTime desc limit 1" -t -u "$USERNAME" > execution.txt
-execution_report="`grep 707* execution.txt`"
-execution_report=`echo "$execution_report" | xargs` # xargs used to remove the ID's leading and trailing whitespaces
-echo The execution ID is "$execution_report"
-(sfdx force:apex:test:report -i "$execution_report" -u "$USERNAME" | sed '/ Pass /d') &> report.txt
+# Run apex tests
+SFDX_MAX_QUERY_LIMIT=30000 sfdx force:apex:test:run --codecoverage --resultformat human -u $USERNAME > exec.txt
+if grep -R -i "Test Run Id" exec.txt
+then
+    execution_id=`grep "Test Run Id" exec.txt | cut -d ' ' -f 13`
+    echo $execution_id
+else
+    echo I am failing in getting the test execution ID. Please contact the release manager.
+    exit 1
+fi
+sfdx force:apex:test:report -i $execution_id -u $USERNAME > report.txt
+
 failed_tests="`grep -c -ow "\bFail\b" report.txt`" # Search for the work "Fail" in report.txt
 num=1 # Variable used as the word "Fail" is also present in the "Fail Rate" metrics; Subtract 1 from the total nr of occurences 
 echo 
 echo You have a total of $[failed_tests - num] failed tests.
+
+# Uncomment if only the Failed Tests are required in the report.
+# sfdx force:apex:test:report -i $execution_report -u $USERNAME > tempreport1.txt
+# sed '/ Pass /d' tempreport1.txt > tempreport2.txt
+# sed 's/Test Results/Failed Tests/' tempreport2.txt > report.txt
+echo
