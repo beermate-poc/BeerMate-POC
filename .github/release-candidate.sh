d0311ae663163ba808607a68e6c8853fbc890662
adrianioana <adi.ioana91@gmail.com>
2022-06-03 15:38:56 +0300

    a

--- .github/release-candidate.sh
+++ .github/release-candidate.sh
@@ -10,8 +10,13 @@ sed 's/feature/release/' feature.txt > rc.txt
 RCBRANCH=`cat rc.txt | grep ^release`
 echo Creating $RCBRANCH from master...
 git checkout origin/master
-git checkout -b $RCBRANCH
-git push $REPOSITORY --set-upstream origin $RCBRANCH
 echo I am currently checked-out on:
 git rev-parse --abbrev-ref HEAD
+echo
+git checkout -b $RCBRANCH
+echo I am currently checked-out on:
+git rev-parse --abbrev-ref HEAD
+echo
+echo Pushing the new branch to remote...
+git push $REPOSITORY --set-upstream origin $RCBRANCH
 echo
