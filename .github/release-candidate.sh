0953202070cb633f4010c319fb54e79053ba7cbe
adrianioana <adi.ioana91@gmail.com>
2022-06-03 13:04:50 +0300

    RC

---
+++ .github/release-candidate.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Created by Adrian Ioana using bash scripting, git, GitHub Actions
+# Last modified date 3rd June 2022
+
+
+echo ================================= Creating a release candidate ==================================
+echo $BRANCH | echo $SQL | sed -e "s/\feature/release/g" > $RCBRANCH
+echo $RCBRANCH
+
+# git checkout origin/master
+# git checkout -b 
