b69f649140ded806daef35026d8780ba54486689
adrianioana <adi.ioana91@gmail.com>
2022-06-03 15:36:22 +0300

    a

--- .github/release-candidate.sh
+++ .github/release-candidate.sh
@@ -8,9 +8,10 @@ echo ================================= Creating a release candidate ============
 echo $BRANCH > feature.txt
 sed 's/feature/release/' feature.txt > rc.txt
 RCBRANCH=`cat rc.txt | grep ^release`
-echo The release candidate branch $RCBRANCH will be created from master...
-# git checkout origin/master
-
-
-# git checkout -b $BRANCH
-# git push $REPOSITORY
+echo Creating $RCBRANCH from master...
+git checkout origin/master
+git checkout -b $RCBRANCH
+git push $REPOSITORY
+echo I am currently checked-out on:
+git rev-parse --abbrev-ref HEAD
+echo
